451e69b434b9d4512b01acb8b51786bf78d11844
Kriper2004xD <fsorokin60@gmail.com>
2023-02-01 18:31:16 +0300

    сообщение

--- public/site.js
+++ public/site.js
@@ -1,29 +1,44 @@
+let myBtn = document.getElementById('add-btn')
+myBtn.addEventListener('click', addItem)
 
+let countTask = 0
 
-  document.addEventListener('keydown', function (event) {
-    if (event.code === 'Enter') {
-      addItem()
-    }
-  })
+function addItem(data) {
+  let myInput = document.getElementById('do-it')
+  let myList = document.getElementById('todo-list')
 
-  let myBtn = document.getElementById('add-btn');
-  myBtn.addEventListener('click', addItem)
-
-  function addItem(data) {
-    let myInput = document.getElementById('do-it')
-    let myList = document.getElementById('todo-list')
-
-    if (myInput.value !== '') {
-      let newItem = document.createElement('li')
-      newItem.innerText = myInput.value;
-
-      myList.appendChild(newItem)
-
-      myInput.value = '';
-
-    }
-
-
-
-    console.log(myInput.value)
+  if (myInput.value !== '') {
+    countTask = countTask + 1;
+    let newItem = document.createElement('li')
+    let newBlock =
+      `<input onclick="finishTask(${countTask})" class="form-check-input me-1"  type="checkbox" value="" id="ch-${countTask}">
+    <label id="${countTask}" class="form-check-label stretched-link" for="ch-${countTask}">${myInput.value}</label>`
+    newItem.innerHTML = newBlock
+    newItem.classList.add('list-group-item')
+    myList.appendChild(newItem)
+    myInput.value = '';
   }
+}
+
+function finishTask(lableId) {
+  let elem = document.getElementById(lableId)
+  elem.classList.add('item-completed')
+}
+
+let allbrow = document.addEventListener('keydown', function (event) {
+  if (event.keyCode === 13) {
+    addItem()
+  }
+})
+
+function finishTask(id) {
+  let text = document.getElementById(id);
+
+  if (text.classList.contains('item-completed')) { text.classList.remove('item-completed'); }
+  else {
+    text.classList.add('item-completed');
+
+  }
+
+
+}
